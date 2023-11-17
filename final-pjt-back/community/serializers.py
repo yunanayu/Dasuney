f6ec8a949e49d3c44c1d2b392e4f11754066d04c
sinoong <qwe6897@naver.com>
2023-11-17 16:21:17 +0900

    community serializers.py

--- final-pjt-back/community/serializers.py
+++ final-pjt-back/community/serializers.py
@@ -1,12 +1,27 @@
 from rest_framework import serializers
 from .models import Review, Comment
 
-class ReviewSerializer(serializers.ModelSerializer):
+class ReviewListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        # 시리얼라이저에 포함할 필드를 지정하세요
+        fields = '__all__'
+
 
 class CommentSerializer(serializers.ModelSerializer):
+    class ReviewSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Review
+            fields = '__all__'
+    review = ReviewSerializer(read_only=True)
+    
     class Meta:
         model = Comment
-        # 시리얼라이저에 포함할 필드를 지정하세요
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    comment_set = CommentSerializer(many=True, read_only=True)
+    comment_count = serializers.IntegerField(source='comment_set.count', read_only=True)
+    class Meta:
+        model = Review
+        fields = '__all__'
