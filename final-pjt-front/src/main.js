e694323b798c5f429c7815660efdd726e81517d9
sinoong <qwe6897@naver.com>
2023-11-20 09:51:25 +0900

    card

--- final-pjt-front/src/main.js
+++ final-pjt-front/src/main.js
@@ -1,20 +1,23 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-// import BootstrapVue from 'bootstrap-vue'
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-// Vue.use(BootstrapVue)
-
-
 import App from './App.vue'
 import router from './router'
+// import 'bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+ 
+// createApp(App).mount('#app')
+
+
+
+
+
+
 
 const app = createApp(App)
 const pinia = createPinia()
 
-pinia.use(piniaPluginPersistedstate)
+// pinia.use(piniaPluginPersistedstate)
 
 
 // app.use(createPinia())
