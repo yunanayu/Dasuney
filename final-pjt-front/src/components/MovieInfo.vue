9cd13434f7ddab0949f7e8bff950804b47d8a2fe
sinoong <qwe6897@naver.com>
2023-11-22 23:12:28 +0900

    star score

--- final-pjt-front/src/components/MovieInfo.vue
+++ final-pjt-front/src/components/MovieInfo.vue
@@ -13,26 +13,18 @@
     <hr>
     <div>
       <h3>평점 주기</h3>
-      <ul>
-        <li><a @click="reRateScore(2)" :style="{ color: scoreColor(2) }">★</a></li>
-        <li><a @click="reRateScore(4)" :style="{ color: scoreColor(4) }">★</a></li>
-        <li><a @click="reRateScore(6)" :style="{ color: scoreColor(6) }">★</a></li>
-        <li><a @click="reRateScore(8)" :style="{ color: scoreColor(8) }">★</a></li>
-        <li><a @click="reRateScore(10)" :style="{ color: scoreColor(10) }">★</a></li>
-      </ul>
       <fieldset class="rate">
-        <input type="radio" id="rating10" name="rating" value="10"><label for="rating10" title="5점"></label>
-        <input type="radio" id="rating9" name="rating" value="9"><label class="half" for="rating9" title="4.5점"></label>
-        <input type="radio" id="rating8" name="rating" value="8"><label for="rating8" title="4점"></label>
-        <input type="radio" id="rating7" name="rating" value="7"><label class="half" for="rating7" title="3.5점"></label>
-        <input type="radio" id="rating6" name="rating" value="6"><label for="rating6" title="3점"></label>
-        <input type="radio" id="rating5" name="rating" value="5"><label class="half" for="rating5" title="2.5점"></label>
-        <input type="radio" id="rating4" name="rating" value="4"><label for="rating4" title="2점"></label>
-        <input type="radio" id="rating3" name="rating" value="3"><label class="half" for="rating3" title="1.5점"></label>
-        <input type="radio" id="rating2" name="rating" value="2"><label for="rating2" title="1점"></label>
-        <input type="radio" id="rating1" name="rating" value="1"><label class="half" for="rating1" title="0.5점"></label>
-
-    </fieldset>
+        <input type="radio" @click="reRateScore(10)" id="rating10" name="rating" value="10"><label for="rating10" title="5점"></label>
+        <input type="radio" @click="reRateScore(9)" id="rating9" name="rating" value="9"><label class="half" for="rating9" title="4.5점"></label>
+        <input type="radio" @click="reRateScore(8)" id="rating8" name="rating" value="8"><label for="rating8" title="4점"></label>
+        <input type="radio" @click="reRateScore(7)" id="rating7" name="rating" value="7"><label class="half" for="rating7" title="3.5점"></label>
+        <input type="radio" @click="reRateScore(6)" id="rating6" name="rating" value="6"><label for="rating6" title="3점"></label>
+        <input type="radio" @click="reRateScore(5)" id="rating5" name="rating" value="5"><label class="half" for="rating5" title="2.5점"></label>
+        <input type="radio" @click="reRateScore(4)" id="rating4" name="rating" value="4"><label for="rating4" title="2점"></label>
+        <input type="radio" @click="reRateScore(3)" id="rating3" name="rating" value="3"><label class="half" for="rating3" title="1.5점"></label>
+        <input type="radio" @click="reRateScore(2)" id="rating2" name="rating" value="2"><label for="rating2" title="1점"></label>
+        <input type="radio" @click="reRateScore(1)" id="rating1" name="rating" value="1"><label class="half" for="rating1" title="0.5점"></label>
+      </fieldset>
     </div>
   </div>
 </template>
