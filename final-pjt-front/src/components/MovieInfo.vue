ab30cb5cdf3dd4cff6249e0a0851899b25e4f678
yuna jeong <jyn7077@naver.com>
2023-11-22 13:41:12 +0900

    inmng

--- final-pjt-front/src/components/MovieInfo.vue
+++ final-pjt-front/src/components/MovieInfo.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <img :src="(`https://image.tmdb.org/t/p/w500/${movieInfo.poster_path}`)" alt="">
+    {{ movieInfo.id }}
     <p>제목 : {{ movieInfo.title }}</p>
     <p>개봉일 : {{ movieInfo.release_date }}</p>
     <p>러닝타임 :{{ movieInfo.runtime }} 분</p>
@@ -9,13 +10,23 @@
     <p v-for="genre in movieInfo.genres">{{ genre.name }}</p>
     <h3>줄거리</h3>
     <p>{{ movieInfo.overview }}</p>
+    <button @click.prevent="hopeMovie(movieInfo.title)">조하여!!!!!!!!!!!!!!!!</button>
   </div>
 </template>
 
 <script setup>
-defineProps({
+import axios from 'axios';
+import { ref,onMounted } from 'vue';
+import { useCounterStore } from '../stores/counter';
+const store = useCounterStore()
+const props = defineProps({
   movieInfo:Object
 })
+const isLiked = ref(false)
+
+const hopeMovie = function (title) {
+  const movie = store
+}
 </script>
 
 <style scoped>
