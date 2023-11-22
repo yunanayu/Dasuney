c8974830df9fa1ac0baf04a689eb6e84877b8afb
sinoong <qwe6897@naver.com>
2023-11-23 00:36:21 +0900

    star

--- final-pjt-front/src/components/MovieInfo.vue
+++ final-pjt-front/src/components/MovieInfo.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
-    <img :src="(`https://image.tmdb.org/t/p/w500/${movieInfo.poster_path}`)" alt="">
+    <img :src="`https://image.tmdb.org/t/p/w500/${movieInfo.poster_path}`" alt="">
     <p>제목 : {{ movieInfo.title }}</p>
     <p>개봉일 : {{ movieInfo.release_date }}</p>
     <p>러닝타임 :{{ movieInfo.runtime }} 분</p>
     <p>TMDB 평점 : {{ movieInfo.vote_average }}</p>
     <h4>장르</h4>
-    <p v-for="genre in movieInfo.genres">{{ genre.name }}</p>
+    <p v-for="genre in movieInfo.genres" :key="genre.id">{{ genre.name }}</p>
     <h3>줄거리</h3>
     <p>{{ movieInfo.overview }}</p>
-    <button @click.prevent="hopeMovie(movieInfo.title)">{{ isLiked ? '안볼래영' : '보고싶어여!!!!!!!!!!!!!!!!'}}</button>
+    <button @click.prevent="hopeMovie">{{ isLiked ? '안볼래영' : '보고싶어여!!!!!!!!!!!!!!!!'}}</button>
     <hr>
     <div>
       <h3>평점 주기</h3>
-      <button @click="reRateScore(0)" :style="{ color: scoreColor(0) }">0점</button>
+      <button @click="cancelRating">평가 취소</button>
       <fieldset class="rate">
         <input type="radio" @click="reRateScore(10)" id="rating10" name="rating" value="10"><label for="rating10" title="5점"></label>
         <input type="radio" @click="reRateScore(9)" id="rating9" name="rating" value="9"><label class="half" for="rating9" title="4.5점"></label>
@@ -33,96 +33,89 @@
 
 <script setup>
 import axios from 'axios';
-import { ref,onMounted } from 'vue';
+import { ref, defineProps, onMounted } from 'vue';
 import { useCounterStore } from '../stores/counter';
-const store = useCounterStore()
-const props = defineProps({
-  movieInfo : Object
-})
-const isLiked = ref(false)
-// 영화 좋아요
-const hopeMovie = function (movietitle) {
-  const movie = store.movies.find((movie) => movie.title === movietitle)
+
+const store = useCounterStore();
+const props = defineProps(['movieInfo']);
+
+const isLiked = ref(false);
+const selectedScore = ref(-1);
+
+const hopeMovie = function () {
+  const movie = store.movies.find((m) => m.title === props.movieInfo.title);
+
   axios({
-    method : 'post',
-    url : `http://127.0.0.1:8000/movies/${movie.id}/movielike/`,
-    headers : {
-      Authorization:`Token ${store.Token}`
+    method: 'post',
+    url: `http://127.0.0.1:8000/movies/${movie.id}/movielike/`,
+    headers: {
+      Authorization: `Token ${store.Token}`
     }
   })
   .then((res) => {
-    // console.log(res.data.is_liked)
-    isLiked.value = res.data.is_liked
+    isLiked.value = res.data.is_liked;
   })
-  .catch(err => console.log(err))
-}
-
+  .catch(err => console.log(err));
+};
 
 const reRateScore = function (score) {
-// 이미 0점이 선택되어 있을 때 0점 버튼을 누르면 선택 해제
-if (selectedScore.value === 0) {
-  selectedScore.value = -1;
-} else {
-  // 만약 선택된 점수가 0이면, 평가가 없음을 나타내기 위해 -1로 설정합니다
-  selectedScore.value = score === 0 ? -1 : score;
-}
+  if (selectedScore.value === 0) {
+    selectedScore.value = -1;
+  } else {
+    selectedScore.value = score === 0 ? -1 : score;
+  }
 
-// 만약 점수가 -1(평가 없음)이면 요청을 보내지 않습니다
-if (selectedScore.value === -1) {
-  console.log("0점으로 평가되지 않습니다.");
-  return;
-}
+  if (selectedScore.value === -1) {
+    console.log("0점으로 평가되지 않습니다.");
+    return;
+  }
 
-const movie = store.movies.find((movie) => movie.title == props.movieInfo.title);
-axios({
-  method: 'post',
-  url: `http://127.0.0.1:8000/movies/${movie.id}/score/`,
-  headers: {
-    Authorization: `Token ${store.Token}`,
-  },
-  data: {
-    score: selectedScore.value,
-  },
-})
+  const movie = store.movies.find((m) => m.title === props.movieInfo.title);
+
+  axios({
+    method: 'post',
+    url: `http://127.0.0.1:8000/movies/${movie.id}/score/`,
+    headers: {
+      Authorization: `Token ${store.Token}`
+    },
+    data: {
+      score: selectedScore.value
+    }
+  })
   .then((res) => {
     console.log(res.data);
   })
   .catch((err) => console.log(err));
 };
 
+const cancelRating = function () {
+  // 평가 취소 버튼을 눌렀을 때 선택된 별 초기화
+  selectedScore.value = -1;
+
+  // 선택된 별이 없다면 초기화하는 로직을 추가할 수 있습니다.
+  const stars = document.querySelectorAll('input[name="rating"]');
+  stars.forEach((star, index) => {
+    star.checked = false;
+  });
+
+  console.log("평가가 취소되었습니다.");
+};
+
 onMounted(() => {
-  const movie = store.movies.find((movie) => movie.title == props.movieInfo.title)
+  const movie = store.movies.find((m) => m.title === props.movieInfo.title);
+
   axios({
-    method : 'get',
-    url : `http://127.0.0.1:8000/movies/${movie.id}/movielike/`,
-    headers : {
-      Authorization:`Token ${store.Token}`
+    method: 'get',
+    url: `http://127.0.0.1:8000/movies/${movie.id}/movielike/`,
+    headers: {
+      Authorization: `Token ${store.Token}`
     }
   })
   .then((res) => {
-    // console.log(res.data.is_liked)
-    isLiked.value = res.data.is_liked
+    isLiked.value = res.data.is_liked;
   })
-  .catch(err => console.log(err))
-})
-
-const scoreColor = (index) => {
-  const filledScore = Math.floor(selectedScore.value);
-
-  if (index <= filledScore) {
-    return 'gold';
-  } else if (index === filledScore + 1 && selectedScore.value % 1 !== 0) {
-    // 반개 별을 위한 부분적으로 채워진 별
-    const gradient = (selectedScore.value % 1) * 100;
-    return `linear-gradient(90deg, gold ${gradient}%, white ${gradient}%)`;
-  } else {
-    return 'white';
-  }
-};
-
-let selectedScore = ref(-1);
-
-
+  .catch(err => console.log(err));
+});
 </script>
 
 <style scoped>
