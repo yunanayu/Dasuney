7fe35c3727a74d59bc646c036dc0bda5dd8938a4
sinoong <qwe6897@naver.com>
2023-11-22 10:55:36 +0900

    hoi

--- final-pjt-front/src/views/accounts/IntroView.vue
+++ final-pjt-front/src/views/accounts/IntroView.vue
@@ -93,6 +93,87 @@
 </script>
 
 <style scoped>
+@media (min-width: 769px) {
+  .bannerposition {
+    left: 100px;
+    top: 250px;
+  }
+
+  .grid-list {
+    grid-gap: 100px;
+  }
+
+  .icon {
+    width: 200px;
+    height: 200px;
+    margin-bottom: 20px;
+  }
+
+  .email-form button {
+    padding: 0 44px;
+    height: 50px;
+    font-size: 18px;
+  }
+
+  .banner {
+    height: 900px;
+  }
+}
+
+@media (min-width: 577px) and (max-width: 768px) {
+  .bannerposition {
+    left: 50px;
+    top: 150px;
+  }
+
+  .grid-list {
+    grid-gap: 50px;
+  }
+
+  .icon {
+    width: 150px;
+    height: 150px;
+    margin-bottom: 15px;
+  }
+
+  .email-form button {
+    padding: 0 30px;
+    height: 40px;
+    font-size: 16px;
+  }
+
+  .banner {
+    height: 600px;
+  }
+}
+
+@media (max-width: 576px) {
+  .bannerposition {
+    left: 20px;
+    top: 100px;
+  }
+
+  .grid-list {
+    grid-gap: 30px;
+  }
+
+  .icon {
+    width: 100px;
+    height: 100px;
+    margin-bottom: 10px;
+  }
+
+  .email-form button {
+    padding: 0 20px;
+    height: 30px;
+    font-size: 14px;
+  }
+
+  .banner {
+    height: 400px;
+  }
+}
+
 .banner {
   position: relative;
   background-image: linear-gradient(to right, rgba(0, 0, 0, 1.0), rgba(0, 0, 0, 0)), url("@/assets/lala.jpg");
@@ -101,6 +182,7 @@
   height: 900px;
   position: relative;
 }
+
 img {
   filter: invert(100%) sepia(0%) saturate(7430%) hue-rotate(148deg) brightness(98%) contrast(101%);
 }
@@ -110,15 +192,7 @@ img {
   z-index: 2;
   white-space: nowrap;
 }
-.bannerposition {
-  position: absolute ;
-  /* left: 100px; */
-  top: 250px;
-  margin-left: 100px;
-}
-.bannertext {
-  margin-bottom: 20px;
-}
+
 .icon-container {
   display: flex;
   justify-content: space-between;
@@ -128,20 +202,20 @@ img {
 
 .icon-item {
   text-align: center;
-
 }
+
 .grid-list {
   display: grid;
-  /* grid-template-columns: repeat(2, 1fr); */
   grid-auto-rows: 300px;
-  grid-gap: 100px;
   padding: 30px;
 }
+
 .icon {
   width: 200px;
   height: 200px;
   margin-bottom: 20px;
 }
+
 .accounts {
   display: grid;
   grid-template-columns: repeat(4, 1fr);
@@ -149,10 +223,12 @@ img {
   grid-gap: 15px;
   padding: 15px;
 }
+
 .dasney {
   font-family: 'disney';
   font-size: 70px;
 }
+
 .email-container {
   width: 100%;
   margin-bottom: 24px;
@@ -191,73 +267,5 @@ img {
 
 h3 {
   color: white;
-}
-
-@media (min-width: 769px) {
-  /* 데스크탑 전용 스타일을 추가할 수 있습니다. */
-}
-
-@media (min-width: 577px) and (max-width: 768px) {
-  /* 태블릿 전용 스타일을 추가할 수 있습니다. */
-  .bannerposition {
-    left: 50px; /* 태블릿용 왼쪽 위치 조정 */
-    top: 150px; /* 태블릿용 상단 위치 조정 */
-  }
-
-  .grid-list {
-    grid-gap: 50px; /* 태블릿용 그리드 간격 조정 */
-  }
-
-  .icon {
-    width: 150px;
-    height: 150px;
-    margin-bottom: 15px;
-  }
-
-  .email-form button {
-    padding: 0 30px;
-    height: 40px;
-    font-size: 16px;
-  }
-
-  /* 태블릿용 이미지 크기 조정 */
-  .banner {
-    height: 600px; /* 이미지 높이 조정 */
-  }
-
-  .icon {
-    width: 120px; /* 이미지 너비 조정 */
-    height: 120px; /* 이미지 높이 조정 */
-    margin-bottom: 10px;
-  }
-}
-
-@media (max-width: 576px) {
-  /* 모바일 전용 스타일 추가 */
-  .bannerposition {
-    left: 20px; /* 모바일용 왼쪽 위치 조정 */
-    top: 100px; /* 모바일용 상단 위치 조정 */
-  }
-
-  .grid-list {
-    grid-gap: 30px; /* 모바일용 그리드 간격 조정 */
-  }
-
-  .icon {
-    width: 100px; /* 이미지 너비 조정 */
-    height: 100px; /* 이미지 높이 조정 */
-    margin-bottom: 10px;
-  }
-
-  .email-form button {
-    padding: 0 20px;
-    height: 30px;
-    font-size: 14px;
-  }
-
-  /* 모바일용 이미지 크기 조정 */
-  .banner {
-    height: 400px; /* 이미지 높이 조정 */
-  }
 }
 </style>
