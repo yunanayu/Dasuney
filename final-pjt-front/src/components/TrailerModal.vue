613bb1a1920dbe1e8449b27648c7a86e0452b3b6
yuna jeong <jyn7077@naver.com>
2023-11-23 15:39:15 +0900

    trailer

--- final-pjt-front/src/components/TrailerModal.vue
+++ final-pjt-front/src/components/TrailerModal.vue
@@ -28,31 +28,45 @@ const closeModal = () => {
   showModal.value = false;
 };
 
-const searchYouTubeTrailer = async () => {
-  try {
-    const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
-      params: {
-        q: `오피셜 트레일러 ${movieId.value}`,  // 필요에 따라 쿼리를 수정하세요
-        part: 'snippet',
-        type: 'video',
-        key: `${store.YOUTUBE_KEY}`,  // 여러분의 YouTube Data API 키로 교체하세요
-      },
-    });
+// const searchYouTubeTrailer = async () => {
+//   try {
+//     const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+//       params: {
+//         q: `오피셜 트레일러 ${movieId.value}`,  // 필요에 따라 쿼리를 수정하세요
+//         part: 'snippet',
+//         type: 'video',
+//         key: `${store.YOUTUBE_KEY}`,  // 여러분의 YouTube Data API 키로 교체하세요
+//       },
+//     });
 
-    if (response.data.items.length > 0) {
-      const videoId = response.data.items[0].id.videoId;
-      youtubeVideoUrl.value = `https://www.youtube.com/embed/${videoId}`;
-    }
-  } catch (error) {
-    console.error('YouTube 트레일러 검색 중 오류 발생:', error);
-  }
-};
+//     if (response.data.items.length > 0) {
+//       const videoId = response.data.items[0].id.videoId;
+//       youtubeVideoUrl.value = `https://www.youtube.com/embed/${videoId}`;
+//     }
+//   } catch (error) {
+//     console.error('YouTube 트레일러 검색 중 오류 발생:', error);
+//   }
+// };
+
+const searchYouTubeTrailer = function () {
+  axios({
+    method :'GET',
+    url : `https://www.googleapis.com/youtube/v3/search/?key=${store.YOUTUBE_KEY}&part=snippet&type=video&q=${movieId.value}`
+  })
+  .then((res) => {
+    console.log(res.data);
+  })
+}
+
 
 // movieId 변경 감지 및 YouTube 검색 트리거
 watch(movieId, searchYouTubeTrailer);
 
 // 선택적: 모달이 닫힐 때 (비디오 URL 지우기 등) 처리할 로직 추가
-onMounted(openModal);
+onMounted(() => {
+  openModal
+
+});
 
 </script>
 
