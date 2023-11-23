db7956ffc02a016e933fb5991a32d89c4850cfb7
yuna <jyn7077@naver.com>
2023-11-24 01:57:12 +0900

    스ㅜ정중 살려 줘

--- final-pjt-front/src/views/ProfileView.vue
+++ final-pjt-front/src/views/ProfileView.vue
@@ -71,6 +71,7 @@ const handleProfilePictureChange = (event) => {
     reader.readAsDataURL(file);
   }
 };
+
 // 팔로우 기능 
 
 const followers = ref([])
@@ -120,6 +121,7 @@ const LikeDirectors = ref([])
 const ratingMovies = ref(0);
 
 onMounted(() => {
+  console.log(route.params.username);
   axios({
     method : 'get',
     url : `http://127.0.0.1:8000/accounts/profile/${route.params.username}/`,
