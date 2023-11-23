0e9c526dbe02fd7e47d4897b653eb2f0f42d68ab
sinoong <qwe6897@naver.com>
2023-11-23 12:17:12 +0900

    as

---
+++ final-pjt-front/src/components/TrailerModal.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="modal" v-if="showModal">
+    <!-- YouTube 트레일러를 표시하기 위한 iframe을 여기에 추가하거나 다른 방법을 사용하세요 -->
+    <iframe width="560" height="315" :src="getYouTubeUrl()" frameborder="0" allowfullscreen></iframe>
+    <button @click="closeModal">닫기</button>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue';
+
+const showModal = ref(false);
+const movieId = ref(null);
+
+const openModal = (id) => {
+  movieId.value = id;
+  showModal.value = true;
+};
+
+const closeModal = () => {
+  movieId.value = null;
+  showModal.value = false;
+};
+
+const getYouTubeUrl = computed(() => {
+  // movieId를 기반으로 YouTube 트레일러 URL을 가져오는 로직을 구현하세요.
+  // 예: return `https://www.youtube.com/embed/${movieId.value}`;
+  return '';
+});
+</script>
+
+<style scoped>
+/* 모달 스타일을 여기에 추가하세요 */
+.modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  padding: 20px;
+  border: 1px solid #ccc;
+}
+</style>
--- final-pjt-front/src/views/HomeView.vue
+++ final-pjt-front/src/views/HomeView.vue
@@ -1,32 +1,16 @@
 <template>
-  <!-- <img class="background-image" src="@/assets/yourname.jpg" alt="yourname">
-  <div class="content"> -->
-
-    <div class="banner">
-        <div class="bannerposition">
-            <h1 class="dasney">Dasuney+ </h1><br>
-            <h1>당신을 위한 영화를 <br>다 줄게요 <br></h1><br>
-            <h5 style="color: gray;">지금 바로 회원이 되어보세요</h5>
-          <div class="accounts">
-            <router-link :to="{ name: 'login' }">
-              <div class="email-container">
-                <form class="email-form">
-                  <div><button>로그인</button></div>
-                </form>
-              </div>
-            </router-link>
-            <router-link :to="{ name: 'signup' }">
-              <div class="email-container">
-                <form class="email-form">
-                  <div><button>회원가입</button></div>
-                </form>
-              </div>
-            </router-link>
-          </div>
-        </div>
-      </div>
-    <MovieList :movies="store.movies"/>
-  <!-- </div> -->
+  <div class="banner">
+    <div class="content">
+      <h1>{{ movieTitle.title }}</h1>
+      <br>
+      <p>시골에 사는 소녀 미츠하(가미시라이시 모네)는 어느 날 잠에서 깬 후 자신의 몸이 남자로 바뀐 걸 알게 된다.<br> 같은 시간, 도쿄에 사는 소년 타키(가미키 류노스케) 역시 이 기이한 상황을 겪고 있다.<br>  언제부턴가 더 이상 몸이 바뀌지 않자 자신들이 특별하게 이어져있었음을 깨달은 타키는 미츠하를 만나러 가는데...</p>
+    </div>
+  </div>
+  <div class="back">
+    <div class="container">
+      <MovieList :movies="store.movies"/>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -44,7 +28,8 @@ onMounted(()=>{
   store.getDirectors()
 
 })
-
+const movieTitle = store.movies.find((movie) => movie.title === '너의 이름은.')
+console.log(movieTitle)
 </script>
 
 
@@ -59,18 +44,29 @@ onMounted(()=>{
   z-index: -1;
 }
 
+.banner {
+  position: relative;
+  background-image: linear-gradient(to top, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0)), url("@/assets/yourname.jpg");
+  background-color: #0b1035;
+  background-size: 1900px 700px;
+  background-repeat: no-repeat;
+  height: 700px;
+  position: relative;
+}
 .content {
   /* 내용을 여기에 추가하세요 */
   padding: 40px;
-  
+  position: absolute;
+  top: 400px;
+  font-size: 15px;
   /* 추가적인 스타일을 필요에 따라 설정하세요 */
 }
-.banner {
-  position: relative;
-  background-image: linear-gradient(to right, rgba(0, 0, 0, 1.0), rgba(0, 0, 0, 0)), url("@/assets/lala.jpg");
-  background-size: cover;
-  background-repeat: no-repeat;
-  height: 900px;
-  position: relative;
+
+.back {
+  background-color: rgb(2, 2, 12);
+}
+
+.container {
+  width: 4000px;
 }
 </style>
